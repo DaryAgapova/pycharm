987228bd580e0ffa20cd80eb4da564d272381328
wcka4kov <wcka4kov2053307@mail.ru>
2023-04-11 10:53:41 +0300

    Ввод элементов массива цикл

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 n = int(input("Введите размер массива: "))
 a = [int(input(f"Введите элемент a[{i}]: ")) for i in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            print(a[i], end=' ')
+        else:
+            print(' ', end=' ')
+    print()
